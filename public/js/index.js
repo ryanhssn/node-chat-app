ed8cedc86e1952b806c32db35b9e407defce6e7f
Bilal H <hussain.bilal000786@gmail.com>
2018-01-17 16:34:36 +0530

    Greet new Users and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,10 @@ var socket = io()
 socket.on('connect', function() {
 	console.log('connected to server');
 
+	// socket.emit('createMessage', {
+	// 	from: 'client@bilal-h.com',
+	// 	text: 'Hey, checking my own written custom chat app.'
+	// })
 });
 
 socket.on('disconnect', function() {
